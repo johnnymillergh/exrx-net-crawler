4f4b68d776793eada628430dd2debe3eab633c3a
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2020-01-19 14:37:06 +0800

    perf($Axios): refine function `restorePathToUrl`

--- src/utils/hyperlink-util.ts
+++ src/utils/hyperlink-util.ts
@@ -1,10 +1,10 @@
 export class HyperlinkUtil {
   static baseUrl = process.env.VUE_APP_EXRX_NET_BASE_URL ? process.env.VUE_APP_EXRX_NET_BASE_URL : 'NONE'
-  static pathPattern = /^\.{2}\/.*$/
+  static pathPattern = /^(\.{0,2}|\\#)\/.*$/
 
   /**
    * Restore path to URL.
-   * @param {string} path, pattern: '../[some-path]'
+   * @param {string} path, pattern: '/[some-path]' or '../[some-path]' or '#/[some-path]'
    * @return URL
    */
   static restorePathToUrl = (path: string): string => {
@@ -12,6 +12,6 @@ export class HyperlinkUtil {
     if (!valid) {
       throw new Error(`Invalid path. Path value: ${path}`)
     }
-    return path.replace(/^\.{2}/, HyperlinkUtil.baseUrl)
+    return path.replace(/^(\.{0,2}|\\#)/, HyperlinkUtil.baseUrl)
   }
 }
